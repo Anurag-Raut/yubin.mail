b815e3a6140cbef890de145d47fbc552411fe1fb
[Anurag Raut] <rautab0@gmail.com>
2025-04-21 14:17:23 +0530

    feat: prod and dev setup

--- main.go
+++ main.go
@@ -1,9 +1,24 @@
 package main
 
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
 func main() {
+	env := os.Getenv("APP_ENV")
+	envFile := "dev.env"
+	if env == "prod" {
+		envFile = ".env"
+	} else {
+		envFile = "dev.env"
+	}
+	godotenv.Load(envFile)
 	cfg := NewConfig()
-	cfg.SetAddr("127.0.0.1")
-	cfg.SetPort("8000")
+
+	cfg.SetAddr(os.Getenv("ADDRESS"))
+	cfg.SetPort(os.Getenv("PORT"))
 	clientServer := NewServer(cfg)
 	clientServer.Listen()
 }
